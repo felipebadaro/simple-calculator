bb019bb0631d6264d02116f01a7c9808c72631de
Felipe Badaro <felipe_badaro@yahoo.com.br>
2023-05-31 00:43:12 -0300

    adds simple version of the operations

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+function add(firstNumber: number, secondNumber: number): void {
+  const result: number = firstNumber + secondNumber;
+  console.log(`The result of ${firstNumber} + ${secondNumber} is: ${result}`);
+}
+
+function sub(firstNumber: number, secondNumber: number): void {
+  const result: number = firstNumber - secondNumber;
+  console.log(`The result of ${firstNumber} - ${secondNumber} is: ${result}`);
+}
+
+function multi(firstNumber: number, secondNumber: number): void {
+  const result: number = firstNumber * secondNumber;
+  console.log(`The result of ${firstNumber} * ${secondNumber} is: ${result}`);
+}
+
+function div(firstNumber: number, secondNumber: number): void {
+  const result: number = firstNumber / secondNumber;
+  console.log(`The result of ${firstNumber} / ${secondNumber} is: ${result}`);
+}
+
+add(2, 2);
+sub(1, 2);
+multi(-1, 2);
+div(4, 2);
